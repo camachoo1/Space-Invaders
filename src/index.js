8bc89eaaa4d69ed0717979ddc3f2c55e80e2c540
“Omar <ocamacho812@gmail.com>
2023-02-09 13:56:50 -0800

    undo changes to test removing event listener

--- src/index.js
+++ src/index.js
@@ -78,8 +78,7 @@ const displayGameOver = function () {
 // Element
 const startGame = document.querySelector('.start-game');
 
-// const start = function () {
-//   setInterval(game, 1000 / 60);
-// };
-// startGame.addEventListener('click', start);
-setInterval(game, 1000 / 60);
+const start = function () {
+  setInterval(game, 1000 / 60);
+};
+startGame.addEventListener('click', start);
